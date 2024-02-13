85148bcd83f1b1a8eb387c733b08af8f0988a2bb
Timothy N <tnelson32@ewu.edu>
2024-02-12 17:26:34 -0800

    Fixed testing environment and proj

--- GenericsHomework/GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -1,14 +1,17 @@
-namespace GenericsHomework.Tests
-{
-    public class NodeTests
-    {
-        /*[Fact]
-        public void DuplicateValueTest()
-        {
-            Node<int> newNode = new(1);
-            Assert.Throws<InvalidOperationException>(() => newNode.Append(1));
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.Metrics;
+using System.IO;
+using Xunit;
 
-        }
-        */
+namespace GenericsHomework.Tests;
+
+[TestClass]
+public class NodeTests
+{
+    [Fact]
+    public void Test1()
+    {
+
     }
 }
