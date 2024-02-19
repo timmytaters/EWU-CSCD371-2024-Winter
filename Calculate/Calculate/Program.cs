66c1797ee0662ae1ad14f63b86314bb129fddb60
Robert Garcia <rgarcia28@ewu.edu>
2024-02-18 22:27:31 -0800

    implemented calculate and program classes

--- Calculate/Calculate/Program.cs
+++ Calculate/Calculate/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Calculate;
+
+public class Program
+{
+
+    // These properties are guaranteed to be non-null because of the 'init' setter,
+    // which mandates that they are initialized either during object instantiation or within a constructor.
+    public Action<string> WriteLine { get; init; } = Console.WriteLine;
+    public Func<string> ReadLine { get; init; } = Console.ReadLine;
+
+    public Program() { }
+
+    public static void Main(string[] args)
+    {
+        Program program = new();
+        Calculator calculator = new();
+        string input;
+        int? answer;
+
+        do
+        {
+            program.WriteLine("Please enter something: ");
+            input = program.ReadLine();
+
+        } while (!calculator.TryCalculate(input, out answer));
+
+        program.WriteLine($"The answer is: {answer}");
+
+    }
+
+}
